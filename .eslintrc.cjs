8c018630234d3c5c1ba1b8a6e67ac3b0e4059f6b
Jason Chen <chenyicai622@qq.com>
2024-07-07 17:24:04 +0800

    feat(配置): 配置调整

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,15 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'no-console': 'error', // 禁止使用 console
-    'no-unused-vars': 'error', // 禁止定义未使用的变量
-    'no-debugger': 'error', // 禁止使用 debugger
-    'no-var': 'error', // 要求使用 let 或 const 而不是 var
-  },
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-console': 1, //'error', // 禁止使用 console
+    'no-unused-vars': 1, //'error', // 禁止定义未使用的变量
+    'no-debugger': 1, //'error', // 禁止使用 debugger
+    'no-var': 'error' // 要求使用 let 或 const 而不是 var
+  }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,78 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { flushSync } from 'react-dom'
 
 function App() {
+  /* const name = 'JC'
+  const list = [1, 2, 3]
+  const flag = false
+  return (
+    <>
+      <p>{name}</p>
+      <p style={{ color: 'red', fontSize: '20px' }}>{name}</p>
+      <p>{flag ? 'jc' : 'cc'}</p>
+      <ul>
+        {list.map((item, index) => {
+          return <li key={index}>{item}</li>
+        })}
+      </ul>
+    </>
+  ) */
+
+  const [name, setName] = useState('JC')
+  const [user, setUser] = useState({ name: 'jc', age: 18 })
+  const [list, setList] = useState([1, 2, 3])
   const [count, setCount] = useState(0)
+
+  function handleClick() {
+    setName('React')
+  }
+
+  function handleUpdateUser() {
+    setUser({ ...user, age: 20 })
+  }
+
+  function handleList() {
+    // 直接push没用，需要创建新的数组
+    setList([...list, 4])
+  }
+
+  function handleCount() {
+    // 上一次值累加
+    //setCount(count => count + 1)
+
+    // 只执行一次
+    /* setCount(count + 1)
+    setCount(count + 1)
+    setCount(count + 1) */
+
+    // setCount异步函数 放到异步函数中转为同步
+    /* setTimeout(() => {
+      setCount(count + 1)
+    }) */
+
+    // 同步更新 但只渲染一次 react18做了合并处理 提升了性能，18之前会渲染两次
+    flushSync(() => {
+      setCount(count => count + 1)
+      setCount(count => count + 1)
+    })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>{name}</p>
+      <p>
+        {user.name}-{user.age}
       </p>
+      <ul>
+        {list.map(item => {
+          return <li key={item}>{item}</li>
+        })}
+      </ul>
+      <p>{count}</p>
+      <button onClick={handleClick}>点击修改</button>
+      <button onClick={handleUpdateUser}>点击用户</button>
+      <button onClick={handleList}>点击列表</button>
+      <button onClick={handleCount}>点击数值</button>
     </>
   )
 }
