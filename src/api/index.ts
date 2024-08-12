4f7e09b382f9cb07dbdc7622365c3973b9801168
chenyicai <chenyicai622@qq.com>
2024-08-12 14:38:55 +0800

    fix(接口): 登录请求修复

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import { Login, User } from '@/types/api'
 
 export default {
   login(params: Login.params): Promise<Login.response> {
-    return request.post('/user/login', params, { showLoading: false })
+    return request.post('/user/login', params, { showLoading: false, showError: true })
   },
   getUserInfo() {
     return request.get<User.UserItem>('/user/getUserInfo')
